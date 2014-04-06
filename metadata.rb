844279c7dc52487508a323d64dd38f81e24b2b23
Vlad Alyukov <valyukov@gmail.com>
2014-04-06 14:01:59 +0400

    lock java/phantomjs cookbook versions

--- metadata.rb
+++ metadata.rb
@@ -2,11 +2,12 @@ name             'selenium'
 maintainer       'Vlad Alyukov'
 maintainer_email 'valyukov@gmail.com'
 license          'Apache 2.0'
-description      'Installs/Configures Selenium/Phantomjs'
-version          '0.4.9'
+description      'Installs/Configures Selenium WebDriver Grid2/Node'
+version          '0.9.26'
 
-%w(debian ubuntu).each do |os|
-  supports os
-end
+supports 'ubuntu', '>= 12.04'
 
-%w{ark phantomjs google-chrome apt java nginx}.each { |cb| depends cb }
+depends 'java', '= 1.22.0'
+depends 'phantomjs', '= 1.0.3'
+
+%w(ark  google-chrome apt chocolatey maven).each { |cb| depends cb }
