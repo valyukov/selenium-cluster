1c4e0105c38effaaa7fcec0243a4c13fb47cb335
Vlad Alyukov <valyukov@gmail.com>
2014-04-27 04:09:35 +0400

    remove test run repition

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
-# require 'chefspec/cacher'
-
-# Require all our libraries
-Dir['spec/recipes/*.rb'].each { |f| require File.expand_path(f) }
+require 'chefspec/cacher'
